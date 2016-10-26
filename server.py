d8de517843bcd389ea726661fe9870b92a34850d
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-25 17:51:21 -0700

    updated routes and templates for user login and registration

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 """Movie Ratings."""
 
 from jinja2 import StrictUndefined
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Rating, Movie
 
 
 app = Flask(__name__)
@@ -22,8 +20,69 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    # a = jsonify([1,3])
+    # return a
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/register", methods=['GET'])
+def register_form():
+    """takes user to registration form"""
+
+    return render_template("register_form.html")
+
+@app.route("/register", methods=['POST'])
+def register_process():
+    """process the registration"""
+
+
+    input_email = request.form.get("user")
+    input_password = request.form.get("password")
+
+    check_user = db.session.query(User).filter_by(email=input_email)
+    if check_user: 
+        print "You are already registered!"
+
+        
+    else:
+        new_user = User(email=input_email, password=input_password)
+
+
+        db.session.add(new_user)
+        db.session.commit()
+       
+
+    #check if user email in database
+    #if not, write email and password into db
+
+    return redirect("/")
+
+@app.route("/login", methods=['POST'])    
+def login():
+    """processes user login and redirects user to homepage after login"""
+
+
+    #create sesion
+    #validate username matches password, add user to session by id 
+    #flash message in base.html
+
+
+    return redirect("/")
+
+@app.route("/logout", methods=['POST'])    
+def logout():
+    """log user out and redirects user to homepage after logout"""
+    # delete user from session
+
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
@@ -38,4 +97,4 @@ if __name__ == "__main__":
 
 
     
-    app.run(port=5003)
+    app.run(port=5000)
